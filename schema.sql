d2d8358ab1f982ff6ecd94f3d2f1f182ea89c6dc
Alexander Goussas <agoussas@espol.edu.ec>
2021-07-21 08:30:10 -0500

    unified schema

--- schema.sql
+++ schema.sql
@@ -1,4 +1,3 @@
-/* Creamos la tabla sonido */
 CREATE TABLE IF NOT EXISTS sonido (
 	PRIMARY KEY (id_sonido),
 	id_sonido   SERIAL       NOT NULL,
@@ -6,14 +5,22 @@ CREATE TABLE IF NOT EXISTS sonido (
 	sonido      BYTEA        NOT NULL
 );
 
-/* Creamos la tabla administrador */
+CREATE TABLE IF NOT EXISTS cliente (
+	PRIMARY KEY (nombre_usuario),
+	nombre_usuario VARCHAR(60)  NOT NULL,
+	contrasena     VARCHAR(55)  NOT NULL,
+	nombre         VARCHAR(60)  NOT NULL,
+	apellido       VARCHAR(60)  NOT NULL,
+	correo         VARCHAR(100) NOT NULL,
+	telefono       VARCHAR(60)  NOT NULL 
+);
+
 CREATE TABLE IF NOT EXISTS administrador (
 	PRIMARY KEY (nombre_usuario),
 	nombre_usuario VARCHAR(50) NOT NULL,
 	constrasena    VARCHAR(50) NOT NULL
 );
 
-/* Creamos la tabla recomendaciones */
 CREATE TABLE IF NOT EXISTS recomendacion (
 	PRIMARY KEY (id_recomendacion),
 	id_recomendacion SERIAL      NOT NULL,
@@ -26,7 +33,6 @@ CREATE TABLE IF NOT EXISTS recomendacion (
 	categoria   VARCHAR(50)  NOT NULL
 );
 
-/* Creamos la tabla recomendacion_cliente */
 CREATE TABLE IF NOT EXISTS recomendacion_cliente (
 	PRIMARY KEY (id_cliente, id_recomendacion),
 	id_cliente VARCHAR(60) NOT NULL,
@@ -40,7 +46,6 @@ CREATE TABLE IF NOT EXISTS recomendacion_cliente (
 	fecha DATE NOT NULL
 );
 
-/* Creamos la tabla tarea */
 CREATE TABLE IF NOT EXISTS tarea (
 	PRIMARY KEY (id_tarea),
 	id_tarea SERIAL NOT NULL,
@@ -48,7 +53,7 @@ CREATE TABLE IF NOT EXISTS tarea (
 	FOREIGN KEY (id_cliente)
 		REFERENCES cliente (nombre_usuario)
 		ON DELETE CASCADE ON UPDATE CASCADE,
-	id_admin VARCHAR(50) NOT NULL,
+	id_admin VARCHAR(50),
 	FOREIGN KEY (id_admin)
 		REFERENCES administrador (nombre_usuario)
 		ON DELETE CASCADE ON UPDATE CASCADE,
@@ -58,4 +63,26 @@ CREATE TABLE IF NOT EXISTS tarea (
 	fecha_fin    DATE         NOT NULL,
 	hora_fin     TIME         NOT NULL,
 	categoria    VARCHAR(50)  NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS recordatorios (
+	PRIMARY KEY (id_recordatorio),
+	id_recordatorio SERIAL NOT NULL,
+	id_tarea INT NOT NULL,
+	FOREIGN KEY (id_tarea)
+		REFERENCES tarea (id_tarea) 
+		ON DELETE CASCADE ON UPDATE CASCADE,
+	id_cliente VARCHAR(60) NOT NULL,
+	FOREIGN KEY (id_cliente)
+		REFERENCES cliente (nombre_usuario) 
+		ON DELETE CASCADE ON UPDATE CASCADE,
+	id_sonido INT NOT NULL,
+	FOREIGN KEY (id_sonido)
+		REFERENCES sonido (id_sonido) 
+		ON DELETE CASCADE ON UPDATE CASCADE,
+	nombre VARCHAR(60) NOT NULL,
+	fecha_inicio DATE  NOT NULL,
+	hora_inicio  TIME  NOT NULL,
+	fecha_fin    DATE  NOT NULL,
+	hora_fin     TIME  NOT NULL
 );
