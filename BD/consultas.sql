0700b0d8f8a310945b8e40244aad95c4133a66c2
Alexander Goussas <agoussas@espol.edu.ec>
2021-07-26 01:05:28 -0500

    consultas iniciales

---
+++ BD/consultas.sql
@@ -0,0 +1,22 @@
+SELECT *
+  FROM sonido;
+
+SELECT *
+  FROM tarea;
+
+SELECT *
+  FROM cliente;
+
+SELECT *
+  FROM administrador;
+
+SELECT *
+  FROM recomendacion;
+
+SELECT *
+  FROM recordatorios;
+
+SELECT *
+  FROM recomendacion_cliente;
+
+
