1a186a231e90dab7f3d2cfbc359b95c83384776e
Alexander Goussas <agoussas@espol.edu.ec>
2021-09-01 19:42:16 -0500

    improved launch script

--- migration.sql
+++ migration.sql
@@ -1,4 +1,6 @@
 -- Set up schema
+\echo 'Setting up schema...'
+
 drop table if exists sonido cascade;
 drop table if exists cliente cascade;
 drop table if exists administrador cascade;
@@ -9,14 +11,26 @@ drop table if exists recordatorios cascade;
 
 \i bd.sql
 
+\echo 'Schema ready!'
+
 -- Load data
+\echo 'Loading data...'
 \i datos.sql
+\echo 'Data loaded!'
 
 -- Create views
+\echo 'Creating views...'
 \i vistas.sql
+\echo 'Views created!'
 
 -- Create procedures
+\echo 'Creating procedures...'
 \i procedimientos.sql
+\echo 'Procedures created!'
 
 -- Prepare triggers
+\echo 'Preparing triggers...'
 \i disparadores.sql
+\echo 'Triggers ready!'
+
+\echo 'Done!'
